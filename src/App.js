4c7cd9d7f1073e22c9b6d03b00a6c5fe5829abfa
sergio rodas <rodascruzsergio@gmail.com>
2022-05-30 22:24:42 -0400

    modificated app in the app css js and index css and js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React from 'react'
+import Result from './components/Result';
+import Functions from './components/Functions';
+import Numbers from './components/Numbers';
+import MathOperations from './components/MathOperations';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <main className="react-calculator">
+      <Result value={undefined} />
+      <Numbers onClickNumber={number => {
+        console.log(number)
+      }}/>
+      <Functions
+        onContentClear={() =>
+          console.log('content clear')}
+        onDelete={() =>
+          console.log('content delete')}
+      />
+      <MathOperations
+        onClickOperation={operation =>
+          console.log(operation)
+        }
+        onClickEqual={equal =>
+          console.log(equal)
+        } />
+    </main>
   );
 }
 
